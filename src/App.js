4a1031bcc0ab3a9ab0e264f88be839b2dfdc5079
Peter Ayello-Wright <peter.ayellowright@gmail.com>
2019-11-05 21:29:29 +0000

    Imported image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import image from "../public/IMG_3041.JPG";
 
 import Countdown from "../src/components/countdown";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/IMG_3041.JPG" alt="Peter and Ellie" />
+        <img src={image} className="App-logo" alt="logo" />
         <h1>Countdown for Pellie</h1>
         <Countdown date="2019-11-30T12:00:00.889Z" />
       </header>
