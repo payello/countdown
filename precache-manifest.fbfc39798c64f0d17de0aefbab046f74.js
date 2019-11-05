7ea61d2336e9313922f799bd5fb1cdab37cbfee4
Peter Ayello-Wright <peter.ayellowright@gmail.com>
2019-11-05 21:11:42 +0000

    Updates

---
+++ precache-manifest.fbfc39798c64f0d17de0aefbab046f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98965f72d1f41b9e54db38e545c7f102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da607f15465c14c8bc1a",
+    "url": "/static/css/main.ed9179bc.chunk.css"
+  },
+  {
+    "revision": "468637f295806062b5cc",
+    "url": "/static/js/2.0cfe9d80.chunk.js"
+  },
+  {
+    "revision": "da607f15465c14c8bc1a",
+    "url": "/static/js/main.30ff2355.chunk.js"
+  },
+  {
+    "revision": "3f5fa5c5be97ca4a6c1d",
+    "url": "/static/js/runtime-main.e8d27f71.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
